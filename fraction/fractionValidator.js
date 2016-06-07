a34ca39c3150bff84de5fdb37a2274eaa4677baf
Stanislava88 <sisiivanovva@gmail.com>
2016-06-07 10:19:09 +0300

    Removed function count()

--- fraction/fractionValidator.js
+++ fraction/fractionValidator.js
@@ -1,13 +1,11 @@
 /**
  * Created by Stanislava on 06.06.16.
  */
-function validateFraction(fieldId) {
+function isFraction(fieldId) {
     var fraction = document.getElementById(fieldId).value;
-    var pattern = /\b\d{1,10}[.]\d{1,5}$\b/;
 
-    var counter = countSeparator(fraction);
-
-    if (!pattern.test(fraction) || counter > 1) {
+    var pattern = new RegExp("^\[0-9]{1,10}([, .][0-9]{1,5})$");
+    if (!pattern.test(fraction)) {
         document.getElementById("message").innerHTML = "This fraction is not valid!!!".fontcolor('red');
         document.getElementById("fraction").style.borderColor = 'red';
         document.getElementById("display").style.display = 'inline';
@@ -16,14 +14,4 @@ function validateFraction(fieldId) {
         document.getElementById("fraction").style.borderColor = 'green';
         document.getElementById("display").style.display = 'none';
     }
-}
-
-function countSeparator(fraction) {
-    var counter = 0;
-    for (var i in fraction) {
-        if (fraction[i] == '.') {
-            counter++;
-        }
-    }
-    return counter;
 }
